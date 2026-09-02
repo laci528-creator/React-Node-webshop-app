f6b21714dcc7a554a241de83703d9cb486df269b
Laszlo Haraszti <laci528@gmail.com>
2026-09-02 17:38:07 +0200

    refactor: clean up database configuration

--- server/src/routes/orders.js
+++ server/src/routes/orders.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
+
 import { createOrder,getMyOrders } from '../controllers/order.controller.js';
+
 import { authenticateToken } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
-// A rendelés leadásához kötelező bejelentkezve lenni (authenticateToken)
+// for creating a new order, the user must be authenticated
 router.post('/', authenticateToken, createOrder);
-
 router.get('/my-orders', authenticateToken, getMyOrders);
 
 export default router;
